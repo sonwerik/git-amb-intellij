82721056534e47cdefc5a0e46cb18fafe650aaaa
sonwerik <sonwerik@gmail.com>
2024-11-26 09:54:13 +0100

    initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println("Addition: " + calculator.add(10, 5));
+        System.out.println("Subtraction: " + calculator.subtract(10, 5));
+    }
+}
